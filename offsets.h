97b2f32e4ccf52259b419c848b9a5c608f69b7e8
ReadProcessMemory <117442701+ReadProcessMemory@users.noreply.github.com>
2023-05-02 12:04:25 +0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,6 +1,6 @@
 namespace offsets
 {
-  uintptr_t uworld = 0x1078A7D8;
-  uintptr_t gnames = 0x10800440;
-  uintptr_t gobjects = 0x105F0AE8;
+  uintptr_t uworld = 0x10567408;
+  uintptr_t gnames = 0x105DC4C0;
+  uintptr_t gobjects = 0x103C93A0;
 }
